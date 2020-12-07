680b892affbf9501b4390be610800a24e009a5b9
fortanna12 <59663200+fortanna12@users.noreply.github.com>
2020-12-07 15:47:36 -0500

    Created Players in the Play Method

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -1,58 +1,101 @@
 /**
- * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
- */
+	 * SYST 17796 Project Base code.
+	 * Students can modify and extend to implement their game.
+	 * Add your name as an author and the date!
+*/
 package ca.sheridancollege.project;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
- * The class that models your game. You should create a more specific child of this class and instantiate the methods
- * given.
- *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
- */
-public abstract class Game {
+	* The class that models your game. You should create a more specific child of this class and instantiate the methods
+	* given.
+	*
+ 	* @author dancye
+ 	* @author Paul Bonenfant Jan 2020
+ 	* @Modifier Jack Farrell
+*/
+public class Game {
 
-    private final String name;//the title of the game
-    private ArrayList<Player> players;// the players of the game
+	    private final String name;//the title of the game
+	    private ArrayList<Player> players;// the players of the game
+	    private static ArrayList<Card> gameDeck;
+	    
+	    
+	    
+		public static void main(String[] args) {			 
+			System.out.println("Game is starting");
+			play();
+			
 
-    public Game(String name) {
-        this.name = name;
-        players = new ArrayList();
-    }
+		}
+		
+	    public Game(String name) {
+	        this.name = name;
+	        players = new ArrayList();
+	    }
 
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
+	    /**
+	     * @return the name
+	     */
+	    public String getName() {
+	        return name;
+	    }
 
-    /**
-     * @return the players of this game
-     */
-    public ArrayList<Player> getPlayers() {
-        return players;
-    }
+	    /**
+	     * @return the players of this game
+	     */
+	    public ArrayList<Player> getPlayers() {
+	        return players;
+	    }
 
-    /**
-     * @param players the players of this game
-     */
-    public void setPlayers(ArrayList<Player> players) {
-        this.players = players;
-    }
+	    /**
+	     * @param players the players of this game
+	     */
+	    public void setPlayers(ArrayList<Player> players) {
+	        this.players = players;
+	    }
 
-    /**
-     * Play the game. This might be one method or many method calls depending on your game.
-     */
-    public abstract void play();
+	    /**
+	     * Play the game. This might be one method or many method calls depending on your game.
+	     */
+	    public static void play() {
+	    	Scanner userIn = new Scanner(System.in);
+	    	System.out.println("Hello and welcome to world poker stars tournament a game by Shawn, Zenan, and Jack");
+	    	System.out.println("Plaese enter your name: ");
+	    	String playerName = userIn.nextLine();
+	    	Player user = new Player(playerName);
+	    	Player opponent = new Player("Bob");
+	    	
+	    	
+	    	
+	    	System.out.println("Hello " + user.getName() + ". Would you like to read the rules before you start?(Y/N)");
+	    	String rulesChoice = userIn.nextLine();
+	    	
+	    	if(rulesChoice.equalsIgnoreCase("Y") || rulesChoice.equalsIgnoreCase("y")) {
+	    		System.out.println("This is a game of five card draw poker. The goal of the game is to have a hand of higher value than your opponent.\n"
+	    				+ "The higher number cards are worth more, and the face card in order of lowest to highest are Jack, Queen, King, and Ace being the higest.\n"
+	    				+ "Your hand increases in value if you have certain cards. The order of those hands from lowest to highest is: \n"
+	    				+ "One Pair, Two Pair, Three of a Kind,Straight(Five Cards in Sequence),Full house (Three of a Kind and one Pair), Four of a Kind, \n"
+	    				+ "Straight Flush (Five Cards in sequence and all in the same suit), and Royal Flush (Ace, King, Queen, Jack, Ten all in the same suit). \n"
+	    				+ "At the beggining of each round, you are dealt five cards, then you may return any or all of those cards for new ones from the deck. \n"
+	    				+ "Then, you may bet coins based on how confident you are that your hand will beat your opponent. Once any betting is finished, \n"
+	    				+ "the cards are revealed. If your hand is more valuable then your opponents, then you win and you get your coins back and any that \n"
+	    				+ "your opponent bet. You may then chose to play another round or leave the table.");
+	    	} else if (rulesChoice.equalsIgnoreCase("N") || rulesChoice.equalsIgnoreCase("n")) {
+	    		System.out.println("Okay now lets get started");
+	    	}
+	    	System.out.println("Okay now lets get started");
+	    }
+	    
 
-    /**
-     * When the game is over, use this method to declare and display a winning player.
-     */
-    public abstract void declareWinner();
+
+	    /**
+	     * When the game is over, use this method to declare and display a winning player.
+	     */
+	    public void declareWinner() {
+	    	
+	    }
 
 }//end class
--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -4,16 +4,20 @@
  * Add your name as an author and the date!
  */
 package ca.sheridancollege.project;
-
+import java.util.ArrayList;
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
  *
  * @author dancye
  * @author Paul Bonenfant Jan 2020
  */
-public abstract class Player {
+public class Player {
 
     private String name; //the unique name for this player
+    private int coins;
+    private ArrayList<Card> hand;
+    private String cardsToSub;
+    
 
     /**
      * A constructor that allows you to set the player's unique ID
@@ -39,11 +43,24 @@ public abstract class Player {
     public void setName(String name) {
         this.name = name;
     }
+    
+      public int getCoins() {
+        return coins;
+    }
+    
+    public void setCoins(int coins) {
+        this.coins = coins;
+    }
+    
+      public ArrayList<Card> getHand() {
+        return hand;
+    }
 
     /**
      * The method to be overridden when you subclass the Player class with your specific type of Player and filled in
      * with logic to play your game.
      */
-    public abstract void play();
+//    public void play() {
+//	}
 
 }
